72c373a4b7c4bbcf2f94f2d3a654f18e78d2568e
Ascen Keeprov <assenkiprov@yahoo.com>
2019-12-11 08:22:33 +0200

    Turned sign in view into functional component

--- Librarium/src/components/registry/registry.jsx
+++ Librarium/src/components/registry/registry.jsx
@@ -7,7 +7,7 @@ export default function Registry() {
 			<section id="registry">
 				<h1>Welcome to Librarium!</h1>
 				<p>Browse our catalogue for the latest book and periodic press issues.</p>
-				<p>Subscribe to get access to high-quality EPUB and PDF content for download and perusal on your preferred device.</p>
+				<p>Subscribe to get access to high-quality EPUB and PDF content for your preferred device.</p>
 			</section>
 		</View>
 	);
--- Librarium/src/components/registry/sign-in.jsx
+++ Librarium/src/components/registry/sign-in.jsx
@@ -3,23 +3,21 @@ import Form from '../../components/shared/form';
 import InputGroup from '../shared/input-group';
 import Kinvey from '../../services/kinvey';
 import { Link } from 'react-router-dom';
-import React from 'react';
+import React, { useContext } from 'react';
 import SessionContext from '../../contexts/session-context';
 import View from '../shared/view';
 import withForm from '../higher-order/with-form';
 
-class SignIn extends React.Component {
-	constructor(props) {
-		super(props);
-		this.signIn = this.signIn.bind(this);
-	}
+function SignIn(props) {
+	const { session } = useContext(SessionContext);
+	const { fields, handleChange, handleSubmit } = props.form;
 
-	signIn(formData) {
+	const signIn = (formData) => {
 		Kinvey.signIn({
 			username: formData.alias,
 			password: formData.password
 		}).then(userData => {
-			this.context.session.authenticate({
+			session.authenticate({
 				authToken: userData._kmd.authtoken,
 				userId: userData._id,
 				username: userData.username
@@ -30,32 +28,27 @@ class SignIn extends React.Component {
 					const userRoles = rolesData
 						.filter(r => roleIds.includes(r._id))
 						.map(r => r.name);
-					this.context.session.authorize(userRoles);
+					session.authorize(userRoles);
 				}).catch(console.error);
 			}
-			this.props.history.push('/');
+			props.history.push('/');
 		}).catch(console.error);
 	}
 
-	render() {
-		const { fields, handleChange, handleSubmit } = this.props.form;
-		return (
-			<View title="Sing In">
-				<Form fields={fields} onSubmit={e => handleSubmit(e, this.signIn)} title="Sign In Form">
-					<fieldset>
-						<InputGroup label="Alias" name="alias" onChange={handleChange} required type="text" value={fields.alias || ''} />
-						<InputGroup label="Password" name="password" onChange={handleChange} required type="password" value={fields.password || ''} />
-					</fieldset>
-					<Button label="Sign In" type="submit" />
-					<small className="float-right">
-						<Link className="text-small" to="/resetpassword">Forgot your password?</Link>
-					</small>
-				</Form>
-			</View>
-		);
-	}
+	return (
+		<View title="Sing In">
+			<Form fields={fields} onSubmit={e => handleSubmit(e, signIn)} title="Sign In Form">
+				<fieldset>
+					<InputGroup label="Alias" name="alias" onChange={handleChange} required type="text" value={fields.alias || ''} />
+					<InputGroup label="Password" name="password" onChange={handleChange} required type="password" value={fields.password || ''} />
+				</fieldset>
+				<Button label="Sign In" type="submit" />
+				<small className="float-right">
+					<Link className="text-small" to="/resetpassword">Forgot your password?</Link>
+				</small>
+			</Form>
+		</View>
+	);
 }
-
-SignIn.contextType = SessionContext;
 
 export default withForm(SignIn);
